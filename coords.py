e1593396a49dd1f62004d0fe9d8b7ebd46b93416
Nicolas VINCENT <nico.vince@gmail.com>
2023-02-21 15:06:56 +0100

    pylint: Adds linter config and fix warnings
    Fixed all warnings from pylint and renamed bar to the proper english
    word 'beam'

--- coords.py
+++ coords.py
@@ -1,16 +1,22 @@
-import logging
+"""Handle coordinates."""
 
 class Coords3D:
+    """Handle 3D coordinates."""
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
 
     def add(self, other):
+        """Adds two instance of Coords3D together.
+
+        Usually this is used to add two vectors, or a position and a vector.
+        """
         assert isinstance(other, type(self))
         return Coords3D(self.x + other.x, self.y + other.y, self.z + other.z)
 
     def is_within(self, start, end):
+        """Check wether a coordinate is inside a bounding box (inclusive)."""
         return (((self.x >= start.x) and (self.x <= end.x))
                 and ((self.y >= start.y) and (self.y <= end.y))
                 and ((self.z >= start.z) and (self.z <= end.z)))
--- piece.py
+++ piece.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
+"""Handle Pieces, blocks and beams"""
 import logging
 from coords import Coords3D
 
 class Block:
+    """Single Cube Block representation."""
     def __init__(self, pos):
         assert isinstance(pos, Coords3D)
         self.pos = pos
         if not self.is_valid():
-            logging.warning(f"Invalid position {self}")
+            logging.warning("Invalid position %s", self)
 
 
     def __repr__(self):
@@ -22,19 +24,21 @@ class Block:
         bb_end = Coords3D(2, 2, 2)
         return self.pos.is_within(bb_start, bb_end)
 
-class Bar:
+class Beam:
+    """Beam holding Block together."""
     def __init__(self, start, vect):
         assert isinstance(start, Coords3D)
         self.start = start
         self.end = start.add(vect)
         self.vect = vect
         if not self.is_valid():
-            logging.warning(f"Invalid bar {self}")
+            logging.warning("Invalid beam %s", self)
 
     def is_valid(self):
+        """Check wether the beam is in a valid position."""
         bb_start = Coords3D(0, 0, 0)
         bb_end = Coords3D(3, 3, 3)
-        return (self.start.is_within(bb_start, bb_end) and self.end.is_within(bb_start, bb_end))
+        return self.start.is_within(bb_start, bb_end) and self.end.is_within(bb_start, bb_end)
 
     def __str__(self):
         return f"start: {self.start} vect:{self.vect}"
@@ -43,47 +47,58 @@ class Bar:
         return f"{type(self).__name__}(start={self.start!r}, vect={self.vect!r})"
 
 class Piece:
-    def __init__(self, blocks, bars):
+    """Collection of Beams and Blocks."""
+    def __init__(self, blocks, beams):
         assert isinstance(blocks, list)
-        assert isinstance(bars, list)
+        assert isinstance(beams, list)
 
-        for b in blocks:
-            assert isinstance(b, Block)
+        for blk in blocks:
+            assert isinstance(blk, Block)
 
-        for b in bars:
-            assert isinstance(b, Bar)
+        for beam in beams:
+            assert isinstance(beam, Beam)
 
         self.blocks = blocks
-        self.bars = bars
+        self.beams = beams
 
     def __repr__(self):
-        return f"{type(self).__name__}(blocks={self.blocks}, bars={self.bars})"
+        return f"{type(self).__name__}(blocks={self.blocks}, beams={self.beams})"
 
     def __str__(self):
-        s = "Blocks:\n"
-        for b in self.blocks:
-            s += " - "
-            s += str(b)
-            s += "\n"
-        s += "Bars:\n"
-        for b in self.bars:
-            s += " - "
-            s += str(b)
-            s += "\n"
-        return s
+        out = "Blocks:\n"
+        for blk in self.blocks:
+            out += " - "
+            out += str(blk)
+            out += "\n"
+        out += "Beams:\n"
+        for beam in self.beams:
+            out += " - "
+            out += str(beam)
+            out += "\n"
+        return out
 
 
     def is_valid(self):
-        """Check wether the current positions of blocks and bars are valid."""
-        pass
+        """Check wether the current positions of blocks and beams are valid."""
+        out = True
+        for blk in self.blocks:
+            out = out and blk.is_valid()
+            if not out:
+                return out
+        for beam in self.beams:
+            out = out and beam.is_valid()
+            if not out:
+                return out
+        return out
 
 
 def main():
-    p1 = Piece([Block(Coords3D(1, 0, 0)), Block(Coords3D(1, 0, 1)), Block(Coords3D(2, 0, 1))],
-               [Bar(Coords3D(0, 0, 1), Coords3D(3, 1, 0)),
-                Bar(Coords3D(1, 1, 0), Coords3D(1, 0, 3))])
-    print(p1)
-    print(p1.blocks[0])
+    """Main function."""
+    piece1 = Piece([Block(Coords3D(1, 0, 0)), Block(Coords3D(1, 0, 1)), Block(Coords3D(2, 0, 1))],
+                   [Beam(Coords3D(0, 0, 1), Coords3D(3, 1, 0)),
+                    Beam(Coords3D(1, 1, 0), Coords3D(1, 0, 3))])
+    print(piece1)
+    print(piece1.blocks[0])
 
 if __name__ == "__main__":
     main()
