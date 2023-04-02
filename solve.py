d9b2f6288ba74cfd3e3f43316a64e439024a88f1
Nicolas VINCENT <nico.vince@gmail.com>
2023-04-02 11:15:56 +0200

    pylint + puzzle: remove pylint hook and add pseudo algo
    pylint version which run in pre commit hook is not the same as local
    one, which makes it horrible because pylintrc syntax is not the same, so
    good bye pylint hook...
    
    Add pseudo algo in py file

---
+++ solve.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+puzzle = []
+add piece to puzzle
+if collision, translate/rotate last piece
+if all positions exhausted remove piece n-1 and translate/rotate that piece
+"""
