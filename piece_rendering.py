5f07231bbd157b9fec5e8d005b916b5b2f22fb69
Julie Digne <julie.digne@liris.cnrs.fr>
2023-04-11 20:41:41 +0200

    render: change cam position
    move camera position to similar place as the one in the reference photography.

--- piece_rendering.py
+++ piece_rendering.py
@@ -1,3 +1,7 @@
+#To be called with 
+# 'blender --background --python piece_rendering.py'
+# will render all pieces in separate files
+
 import bpy
 import sys       # to get command line args
 import argparse  # to parse options for us and print a nice help message
@@ -9,7 +13,6 @@ def render_piece(number):
     pieces = piece.get_pieces()
 
     mypiece = pieces[number]
-    print("coucou")
 
     save_blend=f"/home/julie/perso/nico/cube-puzzle/piece{number}.blend"
     save_png=f"/home/julie/perso/nico/cube-puzzle/piece{number}.png"
@@ -26,7 +29,7 @@ def render_piece(number):
     size = 0.2
     coord = 2*size
     i = 0
-    cubecolor=115/255,72/255,49/255,1
+    cubecolor=204/255,170/255,155/255,1
     mat = bpy.data.materials.new("cubecolor")
     mat.diffuse_color = cubecolor
 
@@ -46,7 +49,7 @@ def render_piece(number):
     i = 0
     sizeb=0.2
     coordb = 2*sizeb
-    beamcolor=51/255,32/255,20/255,1
+    beamcolor=89/255,71/255,53/255,1
     mat2 = bpy.data.materials.new("beamcolor")
     mat2.diffuse_color = beamcolor
     Be = np.zeros((5,5,5), dtype = int)
@@ -65,7 +68,8 @@ def render_piece(number):
 
     V = Bl & Be
 
-    print(f"Max in V: {np.amax(V)}")
+    assert(np.amax(V) == 0)
+
     # Camera
     cam_data = bpy.data.cameras.new("MyCam")
     cam_ob = bpy.data.objects.new(name="MyCam", object_data=cam_data)
@@ -74,17 +78,33 @@ def render_piece(number):
     
     pi = math.pi;
     cam_ob.rotation_mode = 'XYZ'
-    cam_ob.location = 7.3589, -6.9258, 4.9583
-    cam_ob.rotation_euler = 63.6*pi/180, 0*pi/180, 46.7*pi/180
+    ##pos cam 1
+    #cam_ob.location = 7.3589, -6.9258, 4.9583
+    #cam_ob.rotation_euler = 63.6*pi/180, 0*pi/180, 46.7*pi/180
+    ##pos cam 2
+    #cam_ob.location = 4.6027, 10.459, 6.329
+    #cam_ob.rotation_euler = 63.6*pi/180, -0.394*pi/180, -201*pi/180
+    ##pos cam 3
+    cam_ob.location = -3.6678, -5.5478, 4.0441
+    cam_ob.rotation_euler = 64.5*pi/180, -2.9*pi/180, -397*pi/180
     
     # Light
     light_data = bpy.data.lights.new("MyLight", 'POINT')
     light_ob = bpy.data.objects.new(name="MyLight", object_data=light_data)
     scene.collection.objects.link(light_ob)
-    light_ob.location = 4.05, -1.86, 2.7
+    light_ob.location = -3.0447, -2.5547, 2.6529
     light_ob.rotation_euler = 37.3, 3.16, 107
     light_ob.data.energy = 200
     light_ob.data.color= (1,1,1)
+
+
+    light_data2 = bpy.data.lights.new("MyLight2", 'POINT')
+    light_ob2 = bpy.data.objects.new(name="MyLight2", object_data=light_data2)
+    scene.collection.objects.link(light_ob2)
+    light_ob2.location = 2.0506, -3.9892, 1.4709
+    light_ob2.rotation_euler = 37.3, 3.16, 107
+    light_ob2.data.energy = 200
+    light_ob2.data.color= (1,1,1)
     
     bpy.context.view_layer.update()
     
